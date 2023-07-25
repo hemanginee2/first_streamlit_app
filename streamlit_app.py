1391d7389640ce5a7935218ed4acc64b82e99298
HEMANGINEE KARODE <140112347+hemanginee2@users.noreply.github.com>
2023-07-26 02:56:00 +0530

    Update streamlit_app.py
    insert from snowflake

--- streamlit_app.py
+++ streamlit_app.py
@@ -55,7 +55,8 @@ my_cur.execute("insert into fruit_load_list values ('from streamlit') ")
 my_data_rows = my_cur.fetchall()
 streamlit.header("fruit load list contains :")
 streamlit.dataframe(my_data_rows)
-add_my_fruit
+insert into PC_RIVERY_DB.PUBLIC.FRUIT_LOAD_LIST
+values ('jackfruit');
 
 
 
