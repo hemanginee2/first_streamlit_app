771440b8085efada0649c38cde5d49b0434c0049
HEMANGINEE KARODE <140112347+hemanginee2@users.noreply.github.com>
2023-07-25 18:19:27 +0530

    Update streamlit_app.py
    delete raw json add kiwi instead of warwemwlon

--- streamlit_app.py
+++ streamlit_app.py
@@ -36,7 +36,7 @@ streamlit.dataframe( fruits_to_show )
 streamlit.header("Fruityvice Fruit Advice!")
 
 import requests
-fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
+fruityvice_response = requests.get("https://fruityvice.com/api/fruit/kiwi")
 
 # take the json versionof the response and normalize it
 # write your own comment -what does the next line do? 
