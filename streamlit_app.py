f7607830ed240f8041755a43f3d0aa554b52cf3c
HEMANGINEE KARODE <140112347+hemanginee2@users.noreply.github.com>
2023-07-25 22:44:21 +0530

    Update streamlit_app.py
    Input to Fruityvice as Part of the API Call

--- streamlit_app.py
+++ streamlit_app.py
@@ -37,7 +37,7 @@ streamlit.header("Fruityvice Fruit Advice!")
 fruit_choice = streamlit.text_input('What fruit would you like information about?','Kiwi')
 streamlit.write('The user entered ', fruit_choice)
 import requests
-fruityvice_response = requests.get("https://fruityvice.com/api/fruit/fruit_choice")
+fruityvice_response = requests.get("https://fruityvice.com/api/fruit/+fruit_choice")
 
 # take the json versionof the response and normalize it
 # write your own comment -what does the next line do? 
