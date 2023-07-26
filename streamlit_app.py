4c7d6b31beb4e29b893c84b3fa029c2643ec8c86
HEMANGINEE KARODE <140112347+hemanginee2@users.noreply.github.com>
2023-07-26 14:03:45 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -66,6 +66,12 @@ streamlit.text("Hello from Snowflake:")
 streamlit.text(my_data_row)
 
 
+my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
+my_cur = my_cnx.cursor()
+my_cur.execute("SELECT *from fruit_load_list")
+my_data_row = my_cur.fetchone()
+streamlit.header("Hello from Snowflake:")
+streamlit.dataframe(my_data_row)
 
 
 
