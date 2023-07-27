59780a95439ca28cb15d07e5404b1523a6697543
HEMANGINEE KARODE <140112347+hemanginee2@users.noreply.github.com>
2023-07-27 23:44:06 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -4,4 +4,9 @@ streamlit.header( 'Breakfast Menu' )
 streamlit.text( 'Omega 3 & Blueberry Oatmill' )
 streamlit.text( 'Kale Spinach & Rocket Smoothie' )
 streamlit.text( 'Hard-Boiled Free Range Egg' )
+streamlit.header( ' 🥣Breakfast Favorites' )
+streamlit.text( '   🥗Omega 3 & Blueberry Oatmill' )
+streamlit.text( '   🐔Kale Spinach & Rocket Smoothie' )
+streamlit.text( '   🥑🍞Hard-Boiled Free Range Egg' )
+
 
