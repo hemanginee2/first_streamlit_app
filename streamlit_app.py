77475c630b0a23034557d1ba4cd2ae526f20f38f
HEMANGINEE KARODE <140112347+hemanginee2@users.noreply.github.com>
2023-07-28 22:58:29 +0530

    Update streamlit_app.py
    try else except

--- streamlit_app.py
+++ streamlit_app.py
@@ -36,6 +36,8 @@ fruits_to_show = my_fruit_list.loc[fruits_selected]
 # Display the table on the page
 streamlit.dataframe( fruits_to_show )
 
+
+
 # display fruityvice API responce
 streamlit.header("Fruityvice Fruit Advice!")
 fruit_choice = streamlit.text_input('What fruit would you like information about?','Kiwi')
@@ -81,6 +83,20 @@ streamlit.header("Fruityvice Fruit Advice!")
 add_my_fruit = streamlit.text_input('What fruit would you like to add?')
 streamlit.write('Thanks for adding jack fruit ',  add_my_fruit)
 my_cur.execute("insert into fruit_load_list values ('from streamlit')")
+
+
+# new section to display fruityvice API responce
+streamlit.header("Fruityvice Fruit Advice!")
+try:
+        fruit_choice = streamlit.text_input('What Fruit would you like information about?')
+        if not fruit_choice:
+                streamlit.error('Please select a fruit to get information.')
+else:
+          fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + fruit_choice)
+          fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
+          streamlit.dataframe(fruityvice_normalized)
+except URLError as e:
+          streamlit.error()
 
 
 
