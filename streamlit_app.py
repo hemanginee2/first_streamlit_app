b9784ce9e2e470617a3d08c703db86dc271d1544
HEMANGINEE KARODE <140112347+hemanginee2@users.noreply.github.com>
2023-07-29 01:50:16 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -60,7 +60,7 @@ streamlit.dataframe( fruits_to_show )
 def get_fruityvice_data(tghis_fruit_choice):
     fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + this_fruit_choice)
     fruityvice_normalized = pandas.json_normalize(fruityvice_response.json()
-    return fruitvice_normlized
+    return fruitvice_normalized
 try:
   fruit_choice = streamlit.text_input('What fruit would you like information about?','Kiwi')
   if not fruit_choice:
