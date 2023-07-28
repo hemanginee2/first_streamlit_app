6c6ed149bc212816face195cf2b5f1d14ef7a27a
HEMANGINEE KARODE <140112347+hemanginee2@users.noreply.github.com>
2023-07-29 02:01:02 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -75,10 +75,10 @@ try:
 # streamlit.write('The user entered ', fruit_choice)
 
 # import requests
-fruityvice_response = requests.get("https://fruityvice.com/api/fruit/Watermelon")
-fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + "kiwi")
-fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + fruit_choice)
-streamlit.text(fruityvice_response)
+# fruityvice_response = requests.get("https://fruityvice.com/api/fruit/Watermelon")
+# fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + "kiwi")
+# fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + fruit_choice)
+# streamlit.text(fruityvice_response)
 
 #streamlit.text(fruityvice_response.json())
 
