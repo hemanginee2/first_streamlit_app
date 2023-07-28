918efb4ee720348dd58c1c00f13ff0a63cc5979b
HEMANGINEE KARODE <140112347+hemanginee2@users.noreply.github.com>
2023-07-29 02:15:42 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -59,7 +59,7 @@ streamlit.dataframe( fruits_to_show )
 # create the repeatable code block  (called a function)
 def get_fruityvice_data(tghis_fruit_choice):
     fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + this_fruit_choice)
-    fruityvice_normalized = pandas.json_normalize(fruityvice_response.json()
+    fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
     return  fruityvice_normalized
     
 streamlit.header("Fruityvice Fruit Advice!")    
@@ -75,17 +75,17 @@ try:
 # streamlit.write('The user entered ', fruit_choice)
 
 # import requests
-# fruityvice_response = requests.get("https://fruityvice.com/api/fruit/Watermelon")
-# fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + "kiwi")
-# fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + fruit_choice)
-# streamlit.text(fruityvice_response)
+fruityvice_response = requests.get("https://fruityvice.com/api/fruit/Watermelon")
+fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + "kiwi")
+fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + fruit_choice)
+streamlit.text(fruityvice_response)
 
-#streamlit.text(fruityvice_response.json())
+streamlit.text(fruityvice_response.json())
 
-# # write your own comment -what does the next line do? 
-# fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
-# # write your own comment - what does this do?
-# streamlit.dataframe(fruityvice_normalized)
+# write your own comment -what does the next line do? 
+fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
+# write your own comment - what does this do?
+streamlit.dataframe(fruityvice_normalized)
 
 streamlit.stop()
 # # import snowflake.connector
