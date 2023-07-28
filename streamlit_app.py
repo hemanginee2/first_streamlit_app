43009f4311df024d12781f89edfd7f897bcdf8c0
HEMANGINEE KARODE <140112347+hemanginee2@users.noreply.github.com>
2023-07-29 02:17:46 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -76,7 +76,7 @@ try:
 
 # import requests
 # fruityvice_response = requests.get("https://fruityvice.com/api/fruit/Watermelon")
-fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + "kiwi")
+# fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + "kiwi")
 fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + fruit_choice)
 streamlit.text(fruityvice_response)
 
