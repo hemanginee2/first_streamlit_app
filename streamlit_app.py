533b74f9703d83ec4d66e64f3affe60b61514789
HEMANGINEE KARODE <140112347+hemanginee2@users.noreply.github.com>
2023-07-31 22:03:26 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -42,8 +42,8 @@ fruityvice_response = requests.get("https://fruityvice.com/api/fruit/", + fruit_
 # streamlit.text(fruityvice_response)
 # streamlit.text(fruityvice_response.json())
 # take the json version of the response and normalize it
-fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
-streamlit.dataframe(fruityvice_normalized)
+# fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
+# streamlit.dataframe(fruityvice_normalized)
 
 
 
