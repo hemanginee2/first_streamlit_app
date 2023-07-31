7264a99963bcd20706825fdf9746f473a3a1ee6c
HEMANGINEE KARODE <140112347+hemanginee2@users.noreply.github.com>
2023-08-01 04:10:07 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -129,19 +129,19 @@ streamlit.header(" The Fruit Load List Contais:")
 #===========challenge
 # def insert_row_snowflake(new_fruit):
 #     with my_cnx.cursor() as my_cur:
-#         my_cur.execute("insert into fruit_load_list" values (%s), + (new_fruit,))
+#         my_cur.execute("insert into fruit_load_list values (%s),  (new_fruit,))
 #         my_cnx.commit()  
 #         return "Thanks for adding" + new_fruit
 
     
                     
         
-def insert_row_snowflake(new_fruit):
-    with my_cnx.cursor() as my_cur:
-        query = "INSERT INTO fruit_load_list VALUES (%s)"
-        my_cur.execute(query, (new_fruit,))
-        my_cnx.commit()  # Commit the transaction to save changes
-        return "Thanks for adding " + new_fruit
+# def insert_row_snowflake(new_fruit):
+#     with my_cnx.cursor() as my_cur:
+#         query = "INSERT INTO fruit_load_list VALUES (%s)"
+#         my_cur.execute(query, (new_fruit,))
+#         my_cnx.commit()  # Commit the transaction to save changes
+#         return "Thanks for adding " + new_fruit
 
 def add_fruits_to_list():
     # List of fruits to add
@@ -150,7 +150,7 @@ def add_fruits_to_list():
     # Adding fruits to the list in the database
     for fruit in fruits_to_add:
         response = insert_row_snowflake(fruit)
-        print(response)  # Optional: Print the response for each fruit
+        # print(response)  # Optional: Print the response for each fruit
 
 # Call the function to add the fruits to the list
 add_fruits_to_list()
