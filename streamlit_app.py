838fd0e58c1cc89d0b7887b613c27892ae01b5f9
HEMANGINEE KARODE <140112347+hemanginee2@users.noreply.github.com>
2023-08-01 01:00:32 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -40,18 +40,32 @@ fruits_to_show = my_fruit_list.loc[fruits_selected]
 # streamlit.header("Fruityvice Fruit Advice!")
 # fruit_choice = streamlit.text_input('What fruit would you like information about?','Kiwi')
 # streamlit.write('The user entered ', fruit_choice)
+# streamlit.header("Fruityvice Fruit Advice!")
+# try:
+#     fruit_choice = streamlit.text_input('What fruit would you like information about?','Kiwi')
+#     if not fruit_choice:
+#         streamlit.error("Please select a fruit to get information.")
+#     else:
+#         fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + fruit_choice)
+#         fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
+#         streamlit.dataframe(fruityvice_normalized)
+# except URLError as e:
+#     streamlit.error()
+# create a repeatable code block (called a function)
+def get_fruityvice_data(this_fruit_choice):
 streamlit.header("Fruityvice Fruit Advice!")
+fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + this_fruit_choice)
+fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
+# New section to display fruitvicw API Responce
 try:
-    fruit_choice = streamlit.text_input('What fruit would you like information about?','Kiwi')
+    fruit_choice = streamlit.text_input('What fruit would you like information about?')
     if not fruit_choice:
         streamlit.error("Please select a fruit to get information.")
     else:
-        fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + fruit_choice)
-        fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
-        streamlit.dataframe(fruityvice_normalized)
+        back_from_function = get_fruityvice_data(fruit_choice)
+        streamlit.dataframe(back_from_function)
 except URLError as e:
     streamlit.error()
-
     
 
 # streamlit.write('The user entered ', fruit_choice)
@@ -87,6 +101,7 @@ my_data_rows = my_cur.fetchall()
 # my_cur.execute("insert into pc_rivery_db.public.fruit_load_list values ('from streamlit')")
 #=============================
 streamlit.stop()
+
 
 
 
