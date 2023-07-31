c1585d1d903485056ad220b3bb6c93ffa2f67ce3
HEMANGINEE KARODE <140112347+hemanginee2@users.noreply.github.com>
2023-07-31 22:45:08 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -51,9 +51,14 @@ my_cur = my_cnx.cursor()
 # my_cur.execute("SELECT CURRENT_USER(), CURRENT_ACCOUNT(), CURRENT_REGION()")
 my_cur.execute("SELECT *from fruit_load_list")
 my_data_row = my_cur.fetchone()
-streamlit.text("Hello from Snowflake:")
-streamlit.text(" The Fruit Load List Contais:")
-streamlit.text(my_data_row)
+# streamlit.text("Hello from Snowflake:")
+# streamlit.text(" The Fruit Load List Contais:")
+streamlit.header(" The Fruit Load List Contais:")
+# streamlit.text(my_data_row)
+streamlit.dataframe(my_data_row)
+
+
+
 
 
 
