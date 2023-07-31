4622a1da42347a93e3c50733341968eadc850a40
HEMANGINEE KARODE <140112347+hemanginee2@users.noreply.github.com>
2023-08-01 04:15:46 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -129,7 +129,7 @@ streamlit.header(" The Fruit Load List Contais:")
 #===========challenge
 def insert_row_snowflake(new_fruit):
     with my_cnx.cursor() as my_cur:
-        my_cur.execute("insert into fruit_load_list values (%s),(new_fruit,))
+        my_cur.execute("insert into fruit_load_list values (%s),(new_fruit,)")
         my_cnx.commit()  
         return "Thanks for adding" + new_fruit
 
