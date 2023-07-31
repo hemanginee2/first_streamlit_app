6bdbb28fd1376d47ccb6fb8e283a0fe7f57d04c0
HEMANGINEE KARODE <140112347+hemanginee2@users.noreply.github.com>
2023-07-31 23:45:03 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -41,18 +41,18 @@ streamlit.dataframe( fruits_to_show )
 # fruit_choice = streamlit.text_input('What fruit would you like information about?','Kiwi')
 # streamlit.write('The user entered ', fruit_choice)
 streamlit.header("Fruityvice Fruit Advice!")
-try:
-    fruit_choice = streamlit.text_input('What fruit would you like to add?')
-    # if not fruit_choice:
-    #       streamlit.error("Please select a fruit to 
-    if not fruit_choice:
-        streamlit.error("Please select a fruit to get information.")
-    else:
-        fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + fruit_choice)
-        fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
-        streamlit.dataframe(fruityvice_normalized)
-except URLError as e:
-    streamlit.error()
+# try:
+#     fruit_choice = streamlit.text_input('What fruit would you like to add?','kiwi')
+#     # if not fruit_choice:
+#     #       streamlit.error("Please select a fruit to 
+#     if not fruit_choice:
+#         streamlit.error("Please select a fruit to get information.")
+#     else:
+#         fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + fruit_choice)
+#         fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
+#         streamlit.dataframe(fruityvice_normalized)
+# except URLError as e:
+#     streamlit.error()
 
     
 # fruit_choice = streamlit.text_input('What fruit would you like information about?','Kiwi')
@@ -78,11 +78,12 @@ my_data_rows = my_cur.fetchall()
 streamlit.header(" The Fruit Load List Contais:")
 # streamlit.text(my_data_row)
 # streamlit.dataframe(my_data_row)
-streamlit.dataframe(my_data_rows)
-streamlit.stop()
-# fruit_choice = streamlit.text_input('What fruit would you like to add?','Kiwi')
-# streamlit.write('The user entered ', fruit_choice)
-# fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + fruit_choice)
+add_my_fruit = streamlit.text_input("Enter the name of the new fruit:")
+fruit_choice = streamlit.text_input('What fruit would you like to add?','Kiwi')
+streamlit.write('Thanks for adding jackfruits', add_my_fruit)
+fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + fruit_choice)
+
+# streamlit.stop()
 
 
 
