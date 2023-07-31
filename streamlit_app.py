d7566771ad61794f158112bc1abac3905d9fb10c
HEMANGINEE KARODE <140112347+hemanginee2@users.noreply.github.com>
2023-07-31 23:31:25 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -55,8 +55,8 @@ except URLError as e:
     streamlit.error()
 
     
-fruit_choice = streamlit.text_input('What fruit would you like information about?','Kiwi')
-streamlit.write('The user entered ', fruit_choice)
+# fruit_choice = streamlit.text_input('What fruit would you like information about?','Kiwi')
+# streamlit.write('The user entered ', fruit_choice)
 
 # import requests
 # fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
