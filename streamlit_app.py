627f7cd2a30fa13df26fb6a19ac3519c0afa6651
HEMANGINEE KARODE <140112347+hemanginee2@users.noreply.github.com>
2023-08-01 00:31:54 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -41,18 +41,18 @@ streamlit.dataframe( fruits_to_show )
 # fruit_choice = streamlit.text_input('What fruit would you like information about?','Kiwi')
 # streamlit.write('The user entered ', fruit_choice)
 streamlit.header("Fruityvice Fruit Advice!")
-# try:
-#     fruit_choice = streamlit.text_input('What fruit would you like to add?','kiwi')
-#     # if not fruit_choice:
-#     #       streamlit.error("Please select a fruit to 
-#     if not fruit_choice:
-#         streamlit.error("Please select a fruit to get information.")
-#     else:
-#         fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + fruit_choice)
-#         fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
-#         streamlit.dataframe(fruityvice_normalized)
-# except URLError as e:
-#     streamlit.error()
+try:
+    fruit_choice = streamlit.text_input('What fruit would you like to add?','kiwi')
+    # if not fruit_choice:
+    #       streamlit.error("Please select a fruit to 
+    if not fruit_choice:
+        streamlit.error("Please select a fruit to get information.")
+    else:
+        fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + fruit_choice)
+        fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
+        streamlit.dataframe(fruityvice_normalized)
+except URLError as e:
+    streamlit.error()
 
     
 fruit_choice = streamlit.text_input('What fruit would you like information about?','Kiwi')
